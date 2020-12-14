f420da1966c257acb650614ad3c158f185a872b3
ThaiLuu <thailuu9928@gmail.com>
2020-12-14 16:29:23 +0700

    fix

--- src/views/Abouts.vue
+++ src/views/Abouts.vue
@@ -1,103 +1,7 @@
 <template>
   <div class="abouts">
-    <div id="page">
-		<nav class="colorlib-nav" role="navigation">
-			<div class="upper-menu">
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-4">
-							<p>Chào mừng đến với trang web của chúng tôi</p>
-						</div>
-						<div class="col-xs-6 col-md-push-2 text-right">
-							<p>
-								<ul class="colorlib-social-icons">
-									<li><a href="#"><i class="icon-twitter"></i></a></li>
-									<li><a href="#"><i class="icon-facebook"></i></a></li>
-									<li><a href="#"><i class="icon-linkedin"></i></a></li>
-									<li><a href="#"><i class="icon-dribbble"></i></a></li>
-								</ul>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="top-menu">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-2">
-							<div id="colorlib-logo">
-								<a href="index.html">e-learning</a>
-							</div>
-						</div>
-						<div class="col-md-10 text-right menu-1">
-							<ul>
-								<li class="">
-									<a href="index.html">Trang chủ</a>
-								</li>
-								<li class="has-dropdown">
-									<a style="cursor: default">Học tập</a>
-									<ul class="dropdown">
-										<li>
-											<a href="subject.html">Toán học</a>
-										</li>
-										<li>
-											<a href="subject.html">Vật Lý</a>
-										</li>
-										<li>
-											<a href="subject.html">Hoá Học</a>
-										</li>
-										<li>
-											<a href="subject.html">Sinh Học</a>
-										</li>
-										<li>
-											<a href="subject.html">Anh Văn</a>
-										</li>
-										<li>
-											<a href="subject.html">Công Dân</a>
-										</li>
-									</ul>
-								</li>
-								<li class="has-dropdown">
-									<a style="cursor: default">Thi thử</a>
-									<ul class="dropdown">
-										<li>
-											<a href="exam.html">Toán học</a>
-										</li>
-										<li>
-											<a href="exam.html">Vật Lý</a>
-										</li>
-										<li>
-											<a href="exam.html">Hoá Học</a>
-										</li>
-										<li>
-											<a href="exam.html">Sinh Học</a>
-										</li>
-										<li>
-											<a href="exam.html">Anh Văn</a>
-										</li>
-										<li>
-											<a href="exam.html">Công Dân</a>
-										</li>
-									</ul>
-								</li>
-								<li>
-									<a href="news.html">Bài viết mới</a>
-								</li>
-								<li class="active">
-									<a href="about.html">Về chúng tôi</a>
-								</li>
-								<li class=""><a href="contact.html">Liên hệ</a></li>
-								<li class="btn-cta">
-									<a href="auth/index.html">
-										<span>Đăng nhập</span>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
+  <Head-component/>
+		
 		<!-- <aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
@@ -354,7 +258,7 @@
 				</div>
 			</div>
 		</footer>
-	</div>
+	
 
 	<div class="gototop js-top">
 		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
@@ -364,7 +268,9 @@
 </template>
 <script src="src/assets/js/modernizr-2.6.2.min.js"></script>
 <script>
-export default {};
+import HeadComponent from '../components/HeadComponent.vue';
+export default {
+  components: { HeadComponent },};
 </script>
 
 <style>
